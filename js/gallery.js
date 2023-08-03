ed7b0b541d752509be066c57f7b55d0f6217748f
Ubuntu <ubuntu@ip-172-31-62-230.us-west-2.compute.internal>
2023-08-03 23:27:17 +0000

    add preloading for images

--- js/gallery.js
+++ js/gallery.js
@@ -6,13 +6,19 @@ function activateGallery() {
     let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
     
     thumbnails.forEach(thumb => {
+      // preload large images
+      let largeVersion = thumb.dataset.largeVersion;
+      let preloadImage = new Image();
+      preloadImage.setAttribute('src', largeVersion);
+
       thumb.addEventListener("click", function() {
-          mainImage.setAttribute('src', thumb.dataset.largeVersion);
+          // set main image
+          mainImage.setAttribute('src', largeVersion);
           mainImage.setAttribute('alt', thumb.alt);
-          
+          // set current thumbnail
           document.querySelector(".current").classList.remove(currentClass);
           thumb.parentNode.classList.add(currentClass);
-          
+          // set image details
           imageDescription.innerHTML = thumb.dataset.description;
           imageTitle.innerHTML = thumb.dataset.title;
       });
