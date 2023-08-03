8ad9547dee75d5daa9c130346d369e2558186b7d
Ubuntu <ubuntu@ip-172-31-62-230.us-west-2.compute.internal>
2023-08-03 21:56:37 +0000

    update main image/title/description when thumbnail is clicked

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,20 @@
 function activateGallery() {
-    alert('greetings from the gallery.js file!');
+    let currentClass = "current";
+    let imageDescription = document.querySelector('#gallery-info > .description');
+    let imageTitle = document.querySelector('#gallery-info > .title');
+    let mainImage = document.querySelector('#gallery-photo > img');
+    let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
+    
+    thumbnails.forEach(thumb => {
+      thumb.addEventListener("click", function() {
+          mainImage.setAttribute('src', thumb.dataset.largeVersion);
+          mainImage.setAttribute('alt', thumb.alt);
+          
+          document.querySelector(".current").classList.remove(currentClass);
+          thumb.parentNode.classList.add(currentClass);
+          
+          imageDescription.innerHTML = thumb.dataset.description;
+          imageTitle.innerHTML = thumb.dataset.title;
+      });
+    });
 }
